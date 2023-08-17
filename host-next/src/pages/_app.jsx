331526e8c91e065b5b0d8a649a20482143f3131e
Zoltan Feher <zf_common@outlook.com>
2023-08-17 14:50:11 +0200

    chore: unoCSS runtime at work

--- host-next/src/pages/_app.jsx
+++ host-next/src/pages/_app.jsx
@@ -1,4 +1,28 @@
 import App from 'next/app';
+import initUnocssRuntime from '@unocss/runtime';
+import presetWind from '@unocss/preset-wind';
+
+if (typeof window !== 'undefined') {
+  initUnocssRuntime({
+    defaults: {
+      presets: [presetWind()],
+    },
+  });
+
+  // (async () => {
+  //   const [{ default: initUnocssRuntime }, { default: presetWind }] =
+  //     await Promise.all([
+  //       import('@unocss/runtime'),
+  //       import('@unocss/preset-wind'),
+  //     ]);
+
+  //   initUnocssRuntime({
+  //     defaults: {
+  //       presets: [presetWind()],
+  //     },
+  //   });
+  // })();
+}
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- host-next/src/pages/index.jsx
+++ host-next/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import React from 'react';
+import dynamic from 'next/dynamic';
+import React, { Suspense } from 'react';
 // import { printMe } from 'remote1/print';
 // import { getAlma } from 'remote1/alma';
 // import { Title } from 'remote1/title';
@@ -14,6 +15,26 @@ const Title = () => <p>Title</p>;
 // const R1Button = () => <p>R1Button</p>;
 // const R2Button = () => <p>R2Button</p>;
 // const R3Button = () => <p>R3Button</p>;
+
+// const R1Button = dynamic(
+//   async () => {
+//     console.log('R1Button requested');
+//     await new Promise((resolve) => setTimeout(resolve, 1000));
+//     console.log('R1Button importing...');
+//     return import('remote1/button').then((module) => module.Button);
+//   },
+//   { ssr: false },
+// );
+
+// const R3Button = dynamic(
+//   async () => {
+//     console.log('R3Button requested');
+//     await new Promise((resolve) => setTimeout(resolve, 3000));
+//     console.log('R3Button importing...');
+//     return import('remote3/button').then((module) => module.Button);
+//   },
+//   { ssr: false },
+// );
 
 printMe();
 
@@ -34,7 +55,9 @@ const HostNext = () => {
 
         {/* <R2Button></R2Button> */}
 
+        {/* <Suspense> */}
         <R3Button></R3Button>
+        {/* </Suspense> */}
       </div>
     </div>
   );
