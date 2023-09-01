6be9c7f0d8eea636703986fffd4a78ac3fcf9e3b
Zoltan Feher <zf_common@outlook.com>
2023-09-01 15:03:34 +0200

    chore: remove the need of importing React in webpack projects

--- remote2/config/webpack.shared.js
+++ remote2/config/webpack.shared.js
@@ -21,7 +21,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-react'],
+              presets: [['@babel/preset-react', { runtime: 'automatic' }]],
             },
           },
 
--- remote2/src/components/Button.jsx
+++ remote2/src/components/Button.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import '../../remoteEntry/remoteEntry.css';
 
 export const Button = () => {
